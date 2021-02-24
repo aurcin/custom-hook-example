f2821a79ea04140015fbecdde22d2bc977ee05b1
Aurimas <aurcin@gmail.com>
2021-02-24 04:48:50 +0200

    separating fetching into its own component

--- src/components/picture/PictureList.js
+++ src/components/picture/PictureList.js
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import './PictureList.css';
 import Picture from 'components/picture/PictureItem';
 
-const url = 'https://picsum.photos/v2/list';
-
-const PictureList = () => {
-  const [pictures, setPictures] = useState([]);
-
-  useEffect(() => {
-    fetch(url).then(r =>
-      r.json().then(res => {
-        setPictures(res);
-      })
-    );
-  }, []);
-
+const PictureList = ({ pictures }) => {
   const PictureList = pictures.map(({ id, author, download_url }) => {
     return <Picture key={id} author={author} url={download_url} />;
   });
@@ -26,6 +14,10 @@ const PictureList = () => {
       <ul className='picture__list'>{PictureList}</ul>
     </>
   );
+};
+
+PictureList.defaultProps = {
+  pictures: [],
 };
 
 export default PictureList;
